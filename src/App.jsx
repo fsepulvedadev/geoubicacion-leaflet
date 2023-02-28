7252a3ba1c787f8a35c64352dc36b05bf3b9cb02
Francisco Sepulveda <fsepulveda@copade.neuquen.gov.ar>
2023-02-28 10:31:38 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  useMap,
+  Marker,
+  Popup,
+  useMapEvent,
+  useMapEvents,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+function App() {
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
+  const [center, setCenter] = useState([-38.9480448, -68.0624128]);
+
+  useEffect(() => {
+    setInterval(() => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLat(position.coords.latitude);
+        setLng(position.coords.longitude);
+        setCenter([position.coords.latitude, position.coords.longitude]);
+        console.log("Ubicacion actualizada");
+      });
+    }, 100);
+  }, []);
+
+  const MarcadorUbicacion = () => {
+    const map = useMapEvents({
+      click: (e) => {
+        map.locate();
+      },
+      locationfound: (e) => {
+        setLat(e.latitude);
+        setLng(e.longitude);
+        setCenter([e.latitude, e.longitude]);
+        map.flyTo(e.latlng, map.getZoom());
+      },
+    });
+  };
+
+  const mostrarGeo = () => {
+    const newLat = lat + 0.00001;
+    const newLng = lng + 0.00001;
+
+    setLat(newLat);
+    setLng(newLng);
+  };
+
+  const cambiarUbi = (lng, lat) => {
+    setLat(lat);
+    setLng(lng);
+    setCenter([40.4168, -3.7038]);
+  };
+
+  return (
+    <div className="App">
+      <button onClick={mostrarGeo}>Click</button>
+      <h1>Longitud: {lng}</h1>
+      <h1>Latitud: {lat}</h1>
+      <MapContainer center={center} zoom={13} scrollWheelZoom={true}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {lat && lng && (
+          <Marker position={[lat, lng]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        )}
+        <MarcadorUbicacion />
+      </MapContainer>
+    </div>
+  );
+}
+
+export default App;
